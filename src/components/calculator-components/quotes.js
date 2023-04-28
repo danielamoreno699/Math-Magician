64ef6d191424bab1a4315bf4635dedcac98fc7ec
Daniela Moreno <dani9292@hotmail.com>
2023-04-27 20:03:25 -0500

    Add loading and has error useState

--- src/components/calculator-components/quotes.js
+++ src/components/calculator-components/quotes.js
@@ -3,9 +3,12 @@ import React, { useState, useEffect } from 'react';
 const Quotes = () => {
   const category = 'money';
   const [quotes, setQuotes] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       try {
         const response = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
           headers: { 'X-Api-Key': 'cw1sCZ0TspAVMmL1VYR95A==qPbxZI6CH7KNYcWy' },
@@ -17,12 +20,21 @@ const Quotes = () => {
         const data = await response.json();
         setQuotes(data);
       } catch (error) {
-        return error;
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
       }
-      return null;
     };
     fetchData();
   }, [category]);
+
+  if (hasError || quotes.length === 0) {
+    return <div>Something went wrong!</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
