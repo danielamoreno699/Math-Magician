0cf3e89afbe9147f2c19fa568f1c27d1eb38f590
Daniela Moreno <dani9292@hotmail.com>
2023-05-03 18:47:18 -0500

    Completing tests

--- src/___tests___/components/calculator/children.test.js
+++ src/___tests___/components/calculator/children.test.js
@@ -1,22 +1,3 @@
-// import { render, screen, fireEvent } from '@testing-library/react';
-// import CalcComponents  from '../../../components/calculator/children';
-
-// describe('testing calculator', () => {
-//   jest.mock('../../../components/calculator/children');
-
-//   test('when user clicks on the "AC" button, the onClick function should be fired', () => {
-
-//     const { container } = render(<CalcComponents />);
-//     const instance = container.firstChild;
-//     const mockClick = jest.spyOn(instance, 'handleButtonClick').mockImplementation();
-
-//     const acButton = screen.getByRole('button', { name: /AC/i });
-//     fireEvent.click(acButton);
-
-//     expect(mockClick).toHaveBeenCalledTimes(1);
-//   });
-// })
-
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import calculate from '../../../components/logic/caculate';
