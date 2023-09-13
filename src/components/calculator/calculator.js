38c47efd094d3f4d09177eeda501e0b5a145e912
Daniela Moreno <dani9292@hotmail.com>
2023-09-13 17:55:16 -0500

    update css

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -2,7 +2,7 @@ import CalcComponents from './children';
 
 const Calculator = () => (
   <div className="mt-5">
-    <div className="container mt-5">
+    <div className="container">
       <div className="container-calc">
         {/* <div className="col">
           <h2 className="calculator-text">Let&apos;s do some math</h2>
--- src/components/home.js
+++ src/components/home.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 
 const Home = () => (
@@ -8,25 +9,23 @@ const Home = () => (
     </h1>
 
     <p className="p-one mt-4">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-      Repellat repudiandae dolor similique,
-      nulla quam exercitationem,eveniet rem expedita aliquid excepturi
-      dicta aspernatur. Tempora dolorem consequuntur vero minima maiores dicta at?.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-      Repellat repudiandae dolor similique, nulla quam exercitationem,
-      eveniet rem expedita aliquid excepturi dicta aspernatur.
-      Tempora dolorem consequuntur vero minima maiores dicta at?
+      Welcome to the Math Magician app! Here,
+      you can unleash your mathematical prowess with our feature-rich calculator.
+      Whether you're solving simple arithmetic or tackling complex equations,
+      our calculator has you covered.
     </p>
 
     <p className="p-two mt-4">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-      Repellat repudiandae dolor similique, nulla quam exercitationem,
-      eveniet rem expedita aliquid excepturi dicta aspernatur. Tempora dolorem
-      consequuntur vero minima maiores dicta at?.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-      Repellat repudiandae dolor similique, nulla quam exercitationem,
-      eveniet rem expedita aliquid excepturi dicta aspernatur.
-      Tempora dolorem consequuntur vero minima maiores dicta at?
+      But that's not all! In addition to number crunching, you can also indulge in some inspiration.
+      Explore our collection of thought-provoking quotes
+      that will spark your curiosity and ignite your imagination.
+
+    </p>
+
+    <p className="p-three mt-4">
+      So, whether you are here to solve, learn, or simply find motivation,
+      Math Magician has something for everyone.
+      Let\'s embark on a journey of numbers and wisdom together!
     </p>
 
   </div>
