2ea9f0bd761f0b25ff91ced2b187f57e0bb0ac88
Daniela Moreno <dani9292@hotmail.com>
2023-09-13 15:09:04 -0500

    update css

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -3,11 +3,11 @@ import CalcComponents from './children';
 const Calculator = () => (
   <div className="mt-5">
     <div className="container mt-5">
-      <div className="row d-flex justify-content-center">
-        <div className="col">
+      <div className="container-calc">
+        {/* <div className="col">
           <h2 className="calculator-text">Let&apos;s do some math</h2>
-        </div>
-        <div className="col">
+        </div> */}
+        <div className="calc-w">
           <CalcComponents />
         </div>
 
